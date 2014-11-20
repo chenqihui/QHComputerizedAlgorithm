596dfb08dc4da5bda2843e194ba7c9494d244e80
chen <626239153@qq.com>
2014-11-20 15:19:44 +0800

    Update

--- QHComputerizedAlgorithm/MasterViewController.m
+++ QHComputerizedAlgorithm/MasterViewController.m
@@ -11,6 +11,7 @@
 
 @interface MasterViewController ()
 
+@property NSArray *arKey;
 @property NSMutableArray *objects;
 @end
 
@@ -37,15 +38,22 @@
     if (!self.objects) {
         self.objects = [[NSMutableArray alloc] init];
     }
-    [self.objects addObject:@[@"1、数据压缩之游程编码", @"十分简单的无损数据压缩算法"]];
-    [self.objects addObject:@[@"2、数据压缩之位图压缩算法", @"这个算法适用于任何数据结构"]];
-    [self.objects addObject:@[@"3、数据压缩之图编码和模式替换", @"游程编码有两种变形"]];
-    [self.objects addObject:@[@"4、数据压缩之相对编码", @"另一种数据压缩算法"]];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    {
+        NSMutableArray *arTmp = [NSMutableArray new];
+        [arTmp addObject:@[@"1、数据压缩之游程编码", @"十分简单的无损数据压缩算法"]];
+        [arTmp addObject:@[@"2、数据压缩之位图压缩算法", @"这个算法适用于任何数据结构"]];
+        [arTmp addObject:@[@"3、数据压缩之图编码和模式替换", @"游程编码有两种变形"]];
+        [arTmp addObject:@[@"4、数据压缩之相对编码", @"另一种数据压缩算法"]];
+        
+        [self.objects addObject:@[@"一、计算机算法：数据压缩", arTmp]];
+    }
+    {
+        NSMutableArray *arTmp = [NSMutableArray new];
+        [arTmp addObject:@[@"1、栈和队列", @"简单介绍栈(Stack)和队列(Queue)的实现"]];
+        
+        [self.objects addObject:@[@"二、浅谈算法和数据结构", arTmp]];
+    }
 }
 
 - (void)insertNewObject:(id)sender {
@@ -62,27 +70,32 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        [[segue destinationViewController] setDetailItem:@[indexPath, self.objects[indexPath.row]]];
+        NSArray *object = [((NSArray *)[((NSArray *)[self.objects objectAtIndex:indexPath.section]) objectAtIndex:1]) objectAtIndex:indexPath.row];
+        [[segue destinationViewController] setDetailItem:@[indexPath, object]];
     }
 }
 
 #pragma mark - Table View
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return self.objects.count;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.objects.count;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return ((NSArray *)[((NSArray *)[self.objects objectAtIndex:section]) objectAtIndex:1]).count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return (NSString *)[((NSArray *)[self.objects objectAtIndex:section]) objectAtIndex:0];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-//    NSDate *object = self.objects[indexPath.row];
-//    cell.textLabel.text = [object description];
-    
-    NSArray *object = self.objects[indexPath.row];
+    NSArray *object = [((NSArray *)[((NSArray *)[self.objects objectAtIndex:indexPath.section]) objectAtIndex:1]) objectAtIndex:indexPath.row];
     cell.textLabel.text = [object objectAtIndex:0];
     cell.detailTextLabel.text = [object objectAtIndex:1];
     
